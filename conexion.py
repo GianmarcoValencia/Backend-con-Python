2d46733f6d8d787107c91d7ea06d1eedb57286e2
GianmarcoValencia <145680971+GianmarcoValencia@users.noreply.github.com>
2025-12-26 23:01:04 -0500

    Add files via upload

---
+++ conexion.py
@@ -0,0 +1,11 @@
+import pymysql
+
+def obtener_conexion():
+    return pymysql.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="escuela",
+        cursorclass=pymysql.cursors.DictCursor
+    )
+
